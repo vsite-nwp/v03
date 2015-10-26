2aba244657097580bbea59232e95202683d5e3c7
Filip Horvat <fihorvat@vsite.hr>
2015-10-26 23:44:31 +0100

    Variable type POINT and OnInitDialog

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 
 class MyDialog : public Dialog 
 {
+public:
+	POINT cord;
+	
 protected:
 	int IDD()
 	{ 
@@ -12,6 +15,8 @@ protected:
 	bool OnInitDialog()
 	{
 		// TODO: set initial values to edit controls
+		SetInt(IDC_EDIT1, cord.x);
+		SetInt(IDC_EDIT2, cord.y);
 		return true;
 	}
 	bool OnOK()
@@ -27,10 +32,10 @@ protected:
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	// TODO: find current mouse position and transfer to dialog	/pokupi i prosljedi u dijalog
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		// TODO: set mouse position to coordinates from dialog	//setcurpos na te kordinare
 	}
 	return 0;
 }
