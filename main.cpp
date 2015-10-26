ffb3cf019b2f9cd2acb9439d2b7dc4f347415593
kaCulek <karlo2316@gmail.com>
2015-10-26 21:23:48 +0100

    Initital values set...

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 class MyDialog : public Dialog 
 {
+public:
+	POINT p;
 protected:
 	int IDD()
 	{ 
@@ -11,7 +13,9 @@ protected:
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
+		// TODO: set initial values to edit control
+		SetInt(IDC_EDIT1, p.x);
+		SetInt(IDC_EDIT2, p.y);
 		return true;
 	}
 	bool OnOK()
@@ -27,6 +31,7 @@ protected:
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
+	GetCursorPos(&dlg.p);
 	// TODO: find current mouse position and transfer to dialog
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
