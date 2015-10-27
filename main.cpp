203170028bd251842371cdcafca56db6c32c74f6
Filip Horvat <fihorvat@vsite.hr>
2015-10-27 13:21:00 +0100

    without comments

--- main.cpp
+++ main.cpp
@@ -14,19 +14,17 @@ protected:
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
 		SetInt(IDC_EDIT1, cords.x);
 		SetInt(IDC_EDIT2, cords.y);
 		return true;
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
 		try{
 			cords.x = GetInt(IDC_EDIT1);
 			cords.y = GetInt(IDC_EDIT2);
 		}
-		catch (XCtrl&){		// TODO: if not valid return false
+		catch (XCtrl&){	//kom
 			MessageBox(*this, "Only numbers!!", "Try again", MB_OK | MB_ICONEXCLAMATION );
 			return false;
 		}
@@ -40,11 +38,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
 	
-	GetCursorPos(&dlg.cords);		// TODO: find current mouse position and transfer to dialog
+	GetCursorPos(&dlg.cords);		
 
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
 		SetCursorPos(dlg.cords.x, dlg.cords.y);
 	}
 	return 0;
