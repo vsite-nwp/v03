88f1e089cc99b99cb440a1958ad6d63eef64af70
vanjs83 <tihomir.vanjurek@gmail.com>
2015-10-28 10:47:27 +0100

    Set initial values and get values from edit controls try catch bloks

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 class MyDialog : public Dialog 
 {
+public:
+	POINT p;
 protected:
 	int IDD()
 	{ 
@@ -11,13 +13,23 @@ protected:
 	}
 	bool OnInitDialog()
 	{
+		
 		// TODO: set initial values to edit controls
+		SetInt(IDC_EDIT1,p.x);
+		SetInt( IDC_EDIT2,p.y);
 		return true;
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		try {
+			p.x =GetInt( IDC_EDIT1);// TODO: get current values from edit controls
+			p.y = GetInt(IDC_EDIT2);
+		}
+		catch (XCtrl&) {
+			MessageBox(*this, "Upisite samo  brojeve", 0, MB_OK);
+			// TODO: if not valid return false
+			return false;
+		}
 		return true;
 	}
 	void OnCancel()	{ }
@@ -28,9 +40,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
 	// TODO: find current mouse position and transfer to dialog
+	 
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
 		// TODO: set mouse position to coordinates from dialog
+		
 	}
 	return 0;
 }
