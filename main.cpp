52e1e05fb1e9e9f994a06a53e51aaa43f1ebcbc7
vanjs83 <tihomir.vanjurek@gmail.com>
2015-10-29 12:01:49 +0100

    The End TIHOMIR VANJUREK

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ protected:
 	bool OnInitDialog()
 	{
 		
-		// TODO: set initial values to edit controls
+		
 		SetInt(IDC_EDIT1,p.x);
 		SetInt( IDC_EDIT2,p.y);
 		return true;
@@ -22,12 +22,12 @@ protected:
 	bool OnOK()
 	{
 		try {
-			p.x =GetInt( IDC_EDIT1);// TODO: get current values from edit controls
+			p.x =GetInt( IDC_EDIT1);
 			p.y = GetInt(IDC_EDIT2);
 		}
 		catch (XCtrl&) {
 			MessageBox(*this, "Upisite samo  brojeve", 0, MB_OK);
-			// TODO: if not valid return false
+			
 			return false;
 		}
 		return true;
@@ -40,7 +40,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
 	POINT CurPos;
-	// TODO: find current mouse position and transfer to dialog
+
 	 GetCursorPos(&CurPos);
 	   dlg.p.x = CurPos.x;
 	   dlg.p.y = CurPos.y;
@@ -50,7 +50,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		
 		SetCursorPos(dlg.p.x, dlg.p.y);
 	}
 	return 0;
