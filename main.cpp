68ae3a8f807a39a06a30408dca894169d447921f
zugcic <stzugcic@vsite.hr>
2015-10-30 10:07:34 +0100

    Napisana SetCursorPos() funkcija

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include "nwpdlg.h"
 #include "resource.h"
 
+
 class MyDialog : public Dialog 
-{
+{	public:
+	POINT p;
 protected:
 	int IDD()
 	{ 
@@ -11,25 +13,33 @@ protected:
 	}
 	bool OnInitDialog()
 	{
+		SetInt(IDC_EDIT1,p.x);
+		SetInt(IDC_EDIT2,p.y);
 		// TODO: set initial values to edit controls
 		return true;
 	}
 	bool OnOK()
 	{
 		// TODO: get current values from edit controls
+		p.x=GetInt(IDC_EDIT1);
+		p.y=GetInt(IDC_EDIT2);
 		// TODO: if not valid return false
 		return true;
 	}
-	void OnCancel()	{ }
+	void OnCancel()	{  }
 	bool OnCommand(int id, int code) { return false; }
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	GetCursorPos(&dlg.p);// TODO: find current mouse position and transfer to dialog
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
-	{
+	{	
+		int x = dlg.p.x;
+		int y = dlg.p.y;
+
+		SetCursorPos(x,y);
 		// TODO: set mouse position to coordinates from dialog
 	}
 	return 0;
