7bca9d90d47bae1a513468bca0e11b12f6105bb3
Bruno Brckovic <bruno.brckovic@gmail.com>
2017-10-23 21:23:41 +0200

    Zavrsena vjezba

--- main.cpp
+++ main.cpp
@@ -2,22 +2,35 @@
 #include "nwpdlg.h"
 #include "resource.h"
 
-class MyDialog : public Dialog 
+class MyDialog : public Dialog
 {
 protected:
+public:
+	POINT pointer;
+protected:
 	int IDD()
-	{ 
-		return IDD_DIALOG1; 
+	{
+		return IDD_DIALOG1;
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
+		SetInt(IDC_EDIT1, pointer.x);
+		SetInt(IDC_EDIT2, pointer.y);
 		return true;
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		try
+		{
+			pointer.x = GetInt(IDC_EDIT1);
+			pointer.y = GetInt(IDC_EDIT2);
+		}
+
+		catch (XCtrl& a)
+		{
+			return false;
+		}
+
 		return true;
 	}
 	void OnCancel()	{ }
@@ -27,10 +40,10 @@ protected:
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	GetCursorPos(&dlg.pointer);
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.pointer.x, dlg.pointer.y);
 	}
 	return 0;
 }
