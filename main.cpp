3711945e42c9c6958569bc6484e10996a60a2fdf
Marin Pepeljnjak <marin.pepeljnjak@vsite.hr>
2017-10-23 21:24:11 +0200

    gotovo

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 class MyDialog : public Dialog 
 {
+public: 
+	POINT coors;
 protected:
 	int IDD()
 	{ 
@@ -11,13 +13,21 @@ protected:
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
+		SetInt(IDC_EDIT1, coors.x);
+		SetInt(IDC_EDIT2, coors.y);
 		return true;
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		try 
+		{
+			coors.x = GetInt(IDC_EDIT1);
+			coors.y = GetInt(IDC_EDIT2);
+		}
+		catch (XCtrl)
+		{
+			return false;
+		}
 		return true;
 	}
 	void OnCancel()	{ }
@@ -27,9 +37,10 @@ protected:
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	GetCursorPos(&dlg.coors);
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
+		SetCursorPos(dlg.coors.x, dlg.coors.y);
 		// TODO: set mouse position to coordinates from dialog
 	}
 	return 0;
