17ae0977e207696188ad6fad3caa6d028711cf4b
Petar Domitrović <domitrovicpetar@gmail.com>
2017-10-24 15:11:49 +0200

    Rješena vježba 3.

--- main.cpp
+++ main.cpp
@@ -13,15 +13,12 @@ protected:
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
 			SetInt(IDC_EDIT1, coordinates.x);
 			SetInt(IDC_EDIT2, coordinates.y);
 			return true;
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
 		try {
 			coordinates.x =GetInt(IDC_EDIT1);
 			coordinates.y =GetInt(IDC_EDIT2);
@@ -38,11 +35,9 @@ protected:
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
 	GetCursorPos(&dlg.coordinates);
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
 		SetCursorPos(dlg.coordinates.x, dlg.coordinates.y);
 	}
 	return 0;
