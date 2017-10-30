f05618ac02f143dedd1f46d6cd819f8e4bf1f16c
Luka N Sever <lukasever.56@gmail.com>
2017-10-30 18:20:59 +0100

    C.D.

--- main.cpp
+++ main.cpp
@@ -13,19 +13,16 @@ protected:
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
 		SetInt(IDC_EDIT1, xy.x);
 		SetInt(IDC_EDIT2, xy.y);
 		return true;
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
 		try {
 		xy.x = GetInt(IDC_EDIT1);
 		xy.y = GetInt(IDC_EDIT2);
 		}
-		// TODO: if not valid return false
 		catch (XCtrl) {
 			return false;
 		}
@@ -38,11 +35,9 @@ protected:
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
 	GetCursorPos(&dlg.xy);
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
 		SetCursorPos(dlg.xy.x, dlg.xy.y);
 	}
 	return 0;
