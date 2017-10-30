32042cd7dffaeaeb556b4ccedb8de2e7225de62b
Luka N Sever <lukasever.56@gmail.com>
2017-10-30 11:10:29 +0100

    returned LPPOINT to POINT Different use of Get/Set int

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 class MyDialog : public Dialog 
 {
 public:
-	LPPOINT xy;
+	POINT xy;
 protected:
 	int IDD()
 	{ 
@@ -14,15 +14,15 @@ protected:
 	bool OnInitDialog()
 	{
 		// TODO: set initial values to edit controls
-		SetInt(IDC_EDIT1, 0);
-		SetInt(IDC_EDIT2, 0);
+		SetInt(IDC_EDIT1, xy.x);
+		SetInt(IDC_EDIT2, xy.y);
 		return true;
 	}
 	bool OnOK()
 	{
 		// TODO: get current values from edit controls
-		GetInt(IDC_EDIT1);
-		GetInt(IDC_EDIT2);
+		xy.x = GetInt(IDC_EDIT1);
+		xy.y = GetInt(IDC_EDIT2);
 		// TODO: if not valid return false
 		if (GetInt(IDC_EDIT1) == NULL || GetInt(IDC_EDIT2) == NULL)
 			return false;
@@ -36,8 +36,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
 	// TODO: find current mouse position and transfer to dialog
-	//SendDlgItemMessage(dlg.operator HWND, IDD_DIALOG1, GetCursorPos(dlg.xy), NULL, NULL);
-	SetDlgItemInt(dlg.operator HWND, IDD_DIALOG1, GetCursorPos(dlg.xy), true);
+	SendDlgItemMessage(dlg.operator HWND, IDD_DIALOG1, GetCursorPos(dlg.xy), NULL, NULL);
+	//SetDlgItemInt(dlg.operator HWND, IDD_DIALOG1, GetCursorPos(dlg.xy), true);//
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
 		// TODO: set mouse position to coordinates from dialog
