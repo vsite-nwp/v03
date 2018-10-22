2138627792d8ac4b9b85df2df951b1e2e8e35ea5
Mislav <mislav.franja1@gmail.com>
2018-10-22 21:12:58 +0200

    maknuti "TODO"

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ protected:
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
+	
 		SetInt(IDC_EDIT1, mos_pos.x);
 		SetInt(IDC_EDIT2, mos_pos.y);
 
@@ -21,8 +21,6 @@ protected:
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
 
 		try {
 			mos_pos.x = GetInt(IDC_EDIT1);
@@ -41,13 +39,12 @@ protected:
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+
 	GetCursorPos(&dlg.mos_pos);
+	
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
 		SetCursorPos(dlg.mos_pos.x, dlg.mos_pos.y);
-
 	}
 	return 0;
 }
