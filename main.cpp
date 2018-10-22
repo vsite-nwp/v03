f792d711e2ba02cc7f459758e60c7e07a04c8e2f
Mislav <mislav.franja1@gmail.com>
2018-10-22 21:11:51 +0200

    mislim da radi sta treba

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 class MyDialog : public Dialog 
 {
+public:
+	POINT mos_pos;
 protected:
 	int IDD()
 	{ 
@@ -12,12 +14,24 @@ protected:
 	bool OnInitDialog()
 	{
 		// TODO: set initial values to edit controls
+		SetInt(IDC_EDIT1, mos_pos.x);
+		SetInt(IDC_EDIT2, mos_pos.y);
+
 		return true;
 	}
 	bool OnOK()
 	{
 		// TODO: get current values from edit controls
 		// TODO: if not valid return false
+
+		try {
+			mos_pos.x = GetInt(IDC_EDIT1);
+			mos_pos.y = GetInt(IDC_EDIT2);
+		}
+		catch (XCtrl ex) {
+			return false;
+		}
+
 		return true;
 	}
 	void OnCancel()	{ }
@@ -28,9 +42,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
 	// TODO: find current mouse position and transfer to dialog
+	GetCursorPos(&dlg.mos_pos);
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
 		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.mos_pos.x, dlg.mos_pos.y);
+
 	}
 	return 0;
 }
