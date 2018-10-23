0b994521481d7f224992bdfc628afa1c00dadb19
Gordan Sentic <gordan.sentic26@gmail.com>
2018-10-23 13:34:14 +0200

    Vježba 3

--- main.cpp
+++ main.cpp
@@ -4,20 +4,30 @@
 
 class MyDialog : public Dialog 
 {
+public:
+	POINT prebaci;
 protected:
 	int IDD()
 	{ 
 		return IDD_DIALOG1; 
 	}
 	bool OnInitDialog()
-	{
-		// TODO: set initial values to edit controls
+	{	
+		SetInt(IDC_EDIT1, prebaci.x);
+		SetInt(IDC_EDIT2, prebaci.y);
 		return true;
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		try {
+	
+			prebaci.x=GetInt(IDC_EDIT1);
+			prebaci.y=GetInt(IDC_EDIT2);
+		}
+		 catch(XCtrl&){
+			 return false;
+		 }
+		
 		return true;
 	}
 	void OnCancel()	{ }
@@ -27,10 +37,11 @@ protected:
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+		GetCursorPos(&dlg.prebaci);
+
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.prebaci.x,dlg.prebaci.y);
 	}
 	return 0;
 }
