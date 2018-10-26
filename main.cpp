1243be75b89e940e03fdf4b933b08b8ce8ba790f
Matija Akrap <maakrap@gmail.com>
2018-10-26 18:50:46 +0200

    Gotova vjezba

--- main.cpp
+++ main.cpp
@@ -4,20 +4,31 @@
 
 class MyDialog : public Dialog 
 {
-protected:
-	int IDD()
-	{ 
-		return IDD_DIALOG1; 
-	}
-	bool OnInitDialog()
-	{
-		// TODO: set initial values to edit controls
-		return true;
-	}
+	
+	public:
+		POINT mouse;
+	protected:
+		int IDD()
+		{
+			return IDD_DIALOG1;
+		}
+		bool OnInitDialog()
+		{
+			SetInt(IDC_EDIT1, mouse.x);
+			SetInt(IDC_EDIT2, mouse.y);
+			return true;
+		}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		try
+		{
+			GetInt(IDC_EDIT1);
+			GetInt(IDC_EDIT2);
+		}
+		catch (XCtrl&)
+		{
+			return false;
+		}
 		return true;
 	}
 	void OnCancel()	{ }
@@ -27,10 +38,13 @@ protected:
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	GetCursorPos(&dlg.mouse);
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(
+			dlg.mouse.x,
+			dlg.mouse.y
+		);
 	}
 	return 0;
 }
