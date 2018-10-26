98e31cedf493af7d66351aa7f231fd91fc71dd95
Korisnik <Korisnik@LAPTOP-7143TQD9>
2018-10-26 14:46:56 +0200

    1. commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 class MyDialog : public Dialog 
 {
+public:
+	POINT p;
 protected:
 	int IDD()
 	{ 
@@ -11,13 +13,14 @@ protected:
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
+		SetInt(IDC_EDIT1, p.x); 
+		SetInt(IDC_EDIT2, p.y);
 		return true;
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		p.x = GetInt(IDC_EDIT1);
+		p.y = GetInt(IDC_EDIT2);
 		return true;
 	}
 	void OnCancel()	{ }
@@ -26,11 +29,15 @@ protected:
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
+	
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	GetCursorPos(&dlg.p);
+	
+
+	
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.p.x, dlg.p.y);
 	}
 	return 0;
 }
