e3780e9e539321daa423cfdbdfebdbe46a84b3e1
Marko <marko@progis.hr>
2018-10-29 19:07:54 +0100

    Marko Perica

--- main.cpp
+++ main.cpp
@@ -11,26 +11,36 @@ protected:
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
+		SetInt(IDC_EDIT1, point.x);
+		SetInt(IDC_EDIT2, point.y);
 		return true;
 	}
 	bool OnOK()
 	{
 		// TODO: get current values from edit controls
 		// TODO: if not valid return false
+		try {
+			int x = GetInt(point.x);
+			int y = GetInt(point.y);
+		}
+		catch (XCtrl&) { return false; }
 		return true;
 	}
 	void OnCancel()	{ }
 	bool OnCommand(int id, int code) { return false; }
+public:
+	POINT point;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
 	// TODO: find current mouse position and transfer to dialog
+	GetCursorPos(&dlg.point);
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
 		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(&dlg.point.x, &dlg.point.y;);
 	}
 	return 0;
 }
