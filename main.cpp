5fcc96b5fd5b34a3f24548c3799a7382cd9ed109
Igor Obradovic <iobradovic@ZAWMIOB>
2019-10-28 21:04:00 +0100

    Vjezba 3.

--- main.cpp
+++ main.cpp
@@ -11,26 +11,52 @@ protected:
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
+		SetInt(IDC_EDIT1, mPosition.x);
+		SetInt(IDC_EDIT2, mPosition.y);
+
 		return true;
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		try
+		{
+			mPosition.x = GetInt(IDC_EDIT1);
+			mPosition.y = GetInt(IDC_EDIT2);
+		}
+		catch (XCtrl&)
+		{
+			return false;
+		}
 		return true;
 	}
 	void OnCancel()	{ }
 	bool OnCommand(int id, int code) { return false; }
+public:
+	POINT* operator*()
+	{
+		return &mPosition;
+	}
+
+	bool UpdatePos()
+	{
+		if (SetCursorPos(mPosition.x, mPosition.y) == 0)
+		{
+			return false;
+		}
+		return true;
+	}
+private:
+	POINT mPosition;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	GetCursorPos(*dlg);
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		if (dlg.UpdatePos() == 0)
+			return 0;
 	}
 	return 0;
 }
