2a5c2446e3443ba691f41684a8adcd8eb8356294
Sven Gotal <svgotal@vsite.hr>
2019-10-28 21:24:39 +0100

    Implement OnInitDialog(), OnOK(), WinMain()

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 class MyDialog : public Dialog 
 {
+public:
+	POINT poz;
 protected:
 	int IDD()
 	{ 
@@ -11,13 +13,23 @@ protected:
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
+		SetInt(IDC_EDIT1, poz.x);
+		SetInt(IDC_EDIT2, poz.y);
 		return true;
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+
+		try
+		{
+			poz.x = GetInt(IDC_EDIT1);
+			poz.y = GetInt(IDC_EDIT2);
+		}
+		catch (XCtrl)
+		{
+			return FALSE;
+		}
+
 		return true;
 	}
 	void OnCancel()	{ }
@@ -27,10 +39,14 @@ protected:
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	
+	GetCursorPos(&dlg.poz);
+
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.poz.x, dlg.poz.y);
+		EndDialog(dlg, IDD_DIALOG1 );
+
 	}
 	return 0;
 }
