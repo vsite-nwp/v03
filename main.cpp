a12332f393e5f45eb17fd40add7e56fd2d3581e5
Marko Vitković <mavitkov@vsite.hr>
2019-10-30 23:09:46 +0100

    done all

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include "resource.h"
 
 class MyDialog : public Dialog 
-{
+{public: 
+	POINT cur;
 protected:
 	int IDD()
 	{ 
@@ -11,26 +12,37 @@ protected:
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
+		SetInt(IDC_EDIT1, cur.x);
+		SetInt(IDC_EDIT2, cur.y);
 		return true;
+		
 	}
 	bool OnOK()
+
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		try
+		{
+			cur.x = GetInt(IDC_EDIT1);
+			cur.y = GetInt(IDC_EDIT2);
+		}
+		catch (XCtrl) { return false; }
+
 		return true;
+		
 	}
-	void OnCancel()	{ }
+	void OnCancel() {  }
 	bool OnCommand(int id, int code) { return false; }
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	GetCursorPos(&dlg.cur);
+	
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.cur.x, dlg.cur.y);
+		
 	}
 	return 0;
 }
