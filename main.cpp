3962da625c5341c8048da2ebe378ad418953c121
Bruno Akrap <brakrap@vsite.hr>
2019-10-30 11:28:59 +0100

    NWP - vježba 3.

--- main.cpp
+++ main.cpp
@@ -11,26 +11,34 @@ protected:
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
+		SetInt(IDC_EDIT1, p.x);
+		SetInt(IDC_EDIT2, p.y);
 		return true;
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		try {
+			p.x = GetInt(IDC_EDIT1);
+			p.y = GetInt(IDC_EDIT2);
+		}
+		catch {
+			return false;
+		}
 		return true;
 	}
 	void OnCancel()	{ }
 	bool OnCommand(int id, int code) { return false; }
+public:
+	POINT p;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	p = GetCursorPos();
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.p.x, dlg.p.y);
 	}
 	return 0;
 }
