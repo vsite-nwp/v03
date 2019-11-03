7342b2531946ba53c1eaee8d9bb6b96c20926f1c
Sara Cuzele <saracuzele@gmail.com>
2019-11-03 20:56:19 +0100

    v03 - setting cursor position

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 class MyDialog : public Dialog 
 {
+public:
+	POINT cursorPos;
 protected:
 	int IDD()
 	{ 
@@ -11,13 +13,19 @@ protected:
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
+		SetInt(IDC_EDIT1, cursorPos.x);
+		SetInt(IDC_EDIT2, cursorPos.y);
 		return true;
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		try {
+			cursorPos.x = GetInt(IDC_EDIT1);
+			cursorPos.y = GetInt(IDC_EDIT2);
+		}
+		catch (XCtrl&) {
+			return false;
+		}
 		return true;
 	}
 	void OnCancel()	{ }
@@ -27,10 +35,10 @@ protected:
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
-	if(dlg.DoModal(hInstance, NULL) == IDOK)
+	GetCursorPos(&dlg.cursorPos);
+	if (dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.cursorPos.x, dlg.cursorPos.y);
 	}
 	return 0;
 }
