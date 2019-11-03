676876dd7e5a5bf8a32b8566cf1cdb60aeb707b1
brunovsite <brcale@vsite.hr>
2019-11-03 15:37:26 +0100

    vj3

--- main.cpp
+++ main.cpp
@@ -11,13 +11,19 @@ protected:
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
+		SetInt(IDC_EDIT1, pozicija.x);
+		SetInt(IDC_EDIT2, pozicija.y);
 		return true;
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		try {
+			pozicija.x = GetInt(IDC_EDIT1);
+			pozicija.y = GetInt(IDC_EDIT2);
+		}
+		catch(XCtrl){
+			return false;
+		}
 		return true;
 	}
 	void OnCancel()	{ }
@@ -27,10 +33,10 @@ protected:
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	GetCursorPos(&dlg.pozicija);
 	if(dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.pozicija.x, dlg.pozicija.y);
 	}
 	return 0;
 }
