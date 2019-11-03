d84745c661f0353b1872759fc1d814f1efe6870a
tleva <tleva@DESKTOP-J8PKM7S>
2019-11-03 13:58:06 +0100

    Gotova vjezba

--- main.cpp
+++ main.cpp
@@ -2,24 +2,41 @@
 #include "nwpdlg.h"
 #include "resource.h"
 
-class MyDialog : public Dialog 
+class MyDialog : public Dialog
 {
-protected:
-	int IDD()
-	{ 
-		return IDD_DIALOG1; 
-	}
-	bool OnInitDialog()
-	{
-		// TODO: set initial values to edit controls
-		return true;
-	}
-	bool OnOK()
-	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
-		return true;
-	}
+	
+	public:
+		POINT cur;
+	protected:
+		int IDD()
+		{
+			return IDD_DIALOG1;
+		}
+		bool OnInitDialog()
+		{
+			SetInt(IDC_EDIT1, cur.x);
+			SetInt(IDC_EDIT2, cur.y);
+			return true;
+
+		}
+		bool OnOK()
+
+		{
+
+			try
+			{
+				cur.x = GetInt(IDC_EDIT1);
+				cur.y = GetInt(IDC_EDIT2);
+			}
+			catch (XCtrl)
+			{
+				return false;
+			}
+
+			return true;
+
+		}
+	
 	void OnCancel()	{ }
 	bool OnCommand(int id, int code) { return false; }
 };
@@ -27,10 +44,11 @@ protected:
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
-	if(dlg.DoModal(hInstance, NULL) == IDOK)
+	GetCursorPos(&dlg.cur);
+	if (dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.cur.x, dlg.cur.y);
+
 	}
 	return 0;
 }
