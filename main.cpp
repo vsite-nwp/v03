f2ded78d5b0ae3aa06f0ac2181aa8a1c47cd0d85
Ivan Mihin <ivmihin@vsite.hr>
2019-11-04 20:57:43 +0100

    Completed v3.

--- main.cpp
+++ main.cpp
@@ -1,36 +1,65 @@
 #include <windows.h>
 #include "nwpdlg.h"
 #include "resource.h"
-
-class MyDialog : public Dialog 
+class MyDialog : public Dialog
 {
+public:
+	POINT mseCrsr;
 protected:
 	int IDD()
-	{ 
-		return IDD_DIALOG1; 
+	{
+		return IDD_DIALOG1;
 	}
 	bool OnInitDialog()
 	{
-		// TODO: set initial values to edit controls
+		SetInt(IDC_EDIT1, mPosition.x);
+		SetInt(IDC_EDIT2, mPosition.y);
 		return true;
 	}
 	bool OnOK()
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		try
+		{
+			mPosition.x = GetInt(IDC_EDIT1);
+			mPosition.y = GetInt(IDC_EDIT2);
+		}
+		catch (XCtrl&)
+		{
+			return false;
+		}
 		return true;
 	}
-	void OnCancel()	{ }
+	void OnCancel() { }
 	bool OnCommand(int id, int code) { return false; }
+public:
+	POINT* operator*()
+	{
+		return &mPosition;
+	}
+
+	bool UpdatePos()
+	{
+		if (SetCursorPos(mPosition.x, mPosition.y) == 0)
+		{
+			return false;
+		}
+		return true;
+	}
+private:
+	POINT mPosition;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
-	if(dlg.DoModal(hInstance, NULL) == IDOK)
+	GetCursorPos(*dlg);
+
+	if (dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(
+			dlg.mseCrsr.x,
+			dlg.mseCrsr.y
+		);
 	}
 	return 0;
 }
--- nwpdlg.h
+++ nwpdlg.h
@@ -1,18 +1,16 @@
 class XCtrl {};
-
 class Dialog
 {
 	HWND hw;
 public:
 	static int CALLBACK Proc(HWND hw, UINT msg, WPARAM wp, LPARAM lp);
 	operator HWND() const { return hw; }
-	int DoModal(HINSTANCE, HWND parent=0);
+	int DoModal(HINSTANCE, HWND parent = 0);
 protected:
-//	operations
+	//	operations
 	void SetInt(int idCtrl, int val);
 	int GetInt(int idCtrl) throw (XCtrl);
-
-//	overridables
+	//	overridables
 	virtual int IDD() = 0;
 	virtual bool OnInitDialog() { return false; }
 	virtual bool OnCommand(int id, int code) { return false; }
