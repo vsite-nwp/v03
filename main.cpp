e1fb94ba243bde326f01ad61567ccf2d650b7a1c
Ivan <ivan.zoric98@gmail.com>
2019-11-04 20:57:25 +0100

    Solution to vj3.

--- main.cpp
+++ main.cpp
@@ -2,35 +2,47 @@
 #include "nwpdlg.h"
 #include "resource.h"
 
-class MyDialog : public Dialog 
+class MyDialog : public Dialog
 {
-protected:
-	int IDD()
-	{ 
-		return IDD_DIALOG1; 
-	}
-	bool OnInitDialog()
-	{
-		// TODO: set initial values to edit controls
-		return true;
-	}
-	bool OnOK()
-	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
-		return true;
-	}
-	void OnCancel()	{ }
-	bool OnCommand(int id, int code) { return false; }
-};
+	public:
+		POINT p;
+	protected:
+		int IDD()
+		{
+			return IDD_DIALOG1;
+		}
+		bool OnInitDialog()
+		{
+			SetInt(IDC_EDIT1, p.x);
+			SetInt(IDC_EDIT2, p.y);
+			return true;
+
+		}
+		bool OnOK()
+
+		{
+					try	{
+				p.x = GetInt(IDC_EDIT1);
+				p.y = GetInt(IDC_EDIT2);
+			}
+			catch (XCtrl) { return false; }
+
+			return true;
+
+		}
+		void OnCancel() {  }
+		bool OnCommand(int id, int code) { return false; }
+	};
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	MyDialog dlg;
-	// TODO: find current mouse position and transfer to dialog
-	if(dlg.DoModal(hInstance, NULL) == IDOK)
+	GetCursorPos(&dlg.p);
+
+	if (dlg.DoModal(hInstance, NULL) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.p.x, dlg.p.y);
+
 	}
 	return 0;
 }
