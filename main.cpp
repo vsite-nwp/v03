3c5fae3dd6014d84cad0763cb325427d05846f30
tirupcic <tirupcic@vsite.hr>
2021-03-15 21:33:14 +0100

    added runtimeError

--- main.cpp
+++ main.cpp
@@ -1,37 +1,54 @@
 #include <windows.h>
 #include "nwpdlg.h"
 #include "resource.h"
-
-class main_dialog : public vsite::nwp::dialog 
+#include <exception> 
+class main_dialog : public vsite::nwp::dialog
 {
+public:
+	POINT point;
+
 protected:
 	int idd() const override
-	{ 
-		return IDD_DIALOG1; 
+	{
+		return IDD_DIALOG1;
 
 	}
 	bool on_init_dialog() override
 	{
-		// TODO: set initial values to edit controls
+		set_int(IDC_EDIT1, point.x);
+		set_int(IDC_EDIT2, point.y);
+
 		return true;
 	}
 	bool on_ok() override
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		try {
+			point.x = get_int(IDC_EDIT1);
+			point.y = get_int(IDC_EDIT2);
+		}
+		catch (runtime_error& e)
+			return false;
+		}
+
+		//if ((point.x < 0 || point.y < 0)) {
+		//	return false;
+		//}
+
 		return true;
 	}
 	void on_cancel() override { }
 	bool on_command(int id, int code) override { return false; }
 };
 
-int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int nShow)
 {
 	main_dialog dlg;
-	// TODO: find current mouse position and transfer to dialog
-	if(dlg.do_modal(instance, 0) == IDOK)
+	GetCursorPos(&dlg.point);
+
+	//if(dlg.point)
+	if (dlg.do_modal(instance, 0) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.point.x, dlg.point.y);
 	}
 	return 0;
 }
