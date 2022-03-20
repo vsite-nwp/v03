a33ee8734fa49c7936913443014af7b4c4abc902
Vlado Tonković <vltonkov@vsite.hr>
2022-03-20 20:59:45 +0100

    Resolve v03

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <windows.h>
 #include "nwpdlg.h"
 #include "resource.h"
+#include <stdexcept>
 
 class main_dialog : public vsite::nwp::dialog 
 {
+public:
+	POINT p1;
+
 protected:
 	int idd() const override
 	{ 
@@ -11,13 +15,23 @@ protected:
 	}
 	bool on_init_dialog() override
 	{
-		// TODO: set initial values to edit controls
+		set_int(IDC_EDIT1, p1.x);
+		set_int(IDC_EDIT2, p1.y);
 		return true;
 	}
 	bool on_ok() override
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		try
+		{
+			p1.x = get_int(IDC_EDIT1);
+			p1.y = get_int(IDC_EDIT2);
+		}
+
+		catch (std::runtime_error exception)
+		{
+			return false;
+		}
+
 		return true;
 	}
 	void on_cancel() override { }
@@ -27,10 +41,12 @@ protected:
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
 	main_dialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	GetCursorPos(&dlg.p1);
+
 	if(dlg.do_modal(instance, 0) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.p1.x, dlg.p1.y);
 	}
+
 	return 0;
 }
