2eebac94346495a8e514c98bf79a579577d984f8
Filip Lončar <filoncar@vsite.hr>
2022-03-28 13:27:12 +0200

    main stage

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 
 class main_dialog : public vsite::nwp::dialog 
 {
+public:
+	POINT point;
+
+
 protected:
 	int idd() const override
 	{ 
@@ -11,13 +15,20 @@ protected:
 	}
 	bool on_init_dialog() override
 	{
-		// TODO: set initial values to edit controls
+		set_int(IDC_EDIT1, point.x);
+		set_int(IDC_EDIT2, point.y);
 		return true;
 	}
 	bool on_ok() override
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
+		try {
+			point.x = get_int(IDC_EDIT1);
+			point.y = get_int(IDC_EDIT2);
+		}
+		catch (bool) {
+			return false;
+		}
+		
 		return true;
 	}
 	void on_cancel() override { }
@@ -27,10 +38,10 @@ protected:
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
 	main_dialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	if (GetCursorPos(&dlg.point)) {}
 	if(dlg.do_modal(instance, 0) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.point.x, dlg.point.y);
 	}
 	return 0;
 }
