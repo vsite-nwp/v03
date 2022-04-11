cb0572e3febf3c83a4516dfc76677ff05df83c2d
Miomir Lovric <miomir.oe@gmail.com>
2022-04-11 20:59:52 +0200

    V3 gotov

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 class main_dialog : public vsite::nwp::dialog 
 {
+public: POINT kordinate;
+
 protected:
 	int idd() const override
 	{ 
@@ -12,12 +14,16 @@ protected:
 	bool on_init_dialog() override
 	{
 		// TODO: set initial values to edit controls
+		set_int(IDC_EDIT1, kordinate.x);
+		set_int(IDC_EDIT1, kordinate.y);
 		return true;
 	}
 	bool on_ok() override
 	{
 		// TODO: get current values from edit controls
 		// TODO: if not valid return false
+		kordinate.x = get_int(IDC_EDIT1);
+		kordinate.y = get_int(IDC_EDIT2);
 		return true;
 	}
 	void on_cancel() override { }
@@ -28,9 +34,11 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
 	main_dialog dlg;
 	// TODO: find current mouse position and transfer to dialog
+	GetCursorPos(&dlg.kordinate);
 	if(dlg.do_modal(instance, 0) == IDOK)
 	{
 		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.kordinate.x, dlg.kordinate.y);
 	}
 	return 0;
 }
