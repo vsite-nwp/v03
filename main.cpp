1ad00dd79ebbac7c60d9a50343ab3e8f4b194946
Miomir Lovric <miomir.oe@gmail.com>
2022-04-11 21:18:43 +0200

    V3 popravak

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include "nwpdlg.h"
 #include "resource.h"
+#include <exception>
 
 class main_dialog : public vsite::nwp::dialog 
 {
@@ -13,17 +14,23 @@ protected:
 	}
 	bool on_init_dialog() override
 	{
-		// TODO: set initial values to edit controls
+		// : set initial values to edit controls
 		set_int(IDC_EDIT1, kordinate.x);
-		set_int(IDC_EDIT1, kordinate.y);
+		set_int(IDC_EDIT2, kordinate.y);
 		return true;
 	}
 	bool on_ok() override
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
-		kordinate.x = get_int(IDC_EDIT1);
-		kordinate.y = get_int(IDC_EDIT2);
+		// : get current values from edit controls
+		// : if not valid return false
+
+		try {
+			kordinate.x = get_int(IDC_EDIT1);
+			kordinate.y = get_int(IDC_EDIT2);
+		}
+		catch (std::exception &) {
+			return false;
+		}
 		return true;
 	}
 	void on_cancel() override { }
@@ -33,11 +40,11 @@ protected:
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
 	main_dialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	// : find current mouse position and transfer to dialog
 	GetCursorPos(&dlg.kordinate);
 	if(dlg.do_modal(instance, 0) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		// : set mouse position to coordinates from dialog
 		SetCursorPos(dlg.kordinate.x, dlg.kordinate.y);
 	}
 	return 0;
