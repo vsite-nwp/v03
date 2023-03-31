343479173ebff9df426ec343ab675cad3b2cf895
Mihael Vlainic <mihael.vlainic1@gmail.com>
2023-03-31 12:43:55 +0200

    V03

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <windows.h>
 #include "nwpdlg.h"
 #include "resource.h"
+#include <exception>
+
+using namespace std;
 
 class main_dialog : public vsite::nwp::dialog 
 {
-
 public:
 	POINT kordinata;
-	
-
 protected:
 	int idd() const override
 	{ 
@@ -16,18 +16,18 @@ protected:
 	}
 	bool on_init_dialog() override
 	{
-		kordinata.x = 200;
-		kordinata.y = 150;
 		set_int(IDC_EDIT1, kordinata.x);
 		set_int(IDC_EDIT2, kordinata.y);
 		return true;
 	}
 	bool on_ok() override
 	{
-		if (!get_int(IDC_EDIT1) || !get_int(IDC_EDIT2)){return false;}
-		kordinata.x = get_int(IDC_EDIT1);
-		kordinata.y = get_int(IDC_EDIT2);
-		return true;
+		try
+		{
+			kordinata.x = get_int(IDC_EDIT1);
+			kordinata.y = get_int(IDC_EDIT2);
+		}
+		catch (exception& e){return false;}
 	}
 	void on_cancel() override { }
 	bool on_command(int id, int code) override { return false; }
@@ -36,7 +36,7 @@ protected:
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
 	main_dialog dlg;
-	GetCursorPos((LPPOINT) IDD_DIALOG1);
+	GetCursorPos(&dlg.kordinata);
 	if(dlg.do_modal(instance, 0) == IDOK)
 	{
 		SetCursorPos(dlg.kordinata.x, dlg.kordinata.y);
