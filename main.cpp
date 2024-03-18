6a2204c06bb0792f45383e0fa1b5b7f3192e5da0
Kristian Bolarić <krbolaric@vsite.hr>
2024-03-18 20:53:31 +0100

    Vjezba 3

--- main.cpp
+++ main.cpp
@@ -2,35 +2,50 @@
 #include "nwpdlg.h"
 #include "resource.h"
 
-class main_dialog : public vsite::nwp::dialog 
+class main_dialog : public vsite::nwp::dialog
 {
+public:
+	POINT coords;
+
 protected:
 	int idd() const override
-	{ 
-		return IDD_DIALOG1; 
+	{
+		return IDD_DIALOG1;
 	}
+
 	bool on_init_dialog() override
 	{
-		// TODO: set initial values to edit controls
+		set_int(IDC_EDIT1, coords.x);
+		set_int(IDC_EDIT2, coords.y);
 		return true;
 	}
+
 	bool on_ok() override
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
-		return true;
+		try {
+			coords.x = get_int(IDC_EDIT1);
+			coords.y = get_int(IDC_EDIT2);
+			return true;
+		}
+		catch (...) {
+			return false;
+		}
 	}
+
 	void on_cancel() override { }
+
 	bool on_command(int id, int code) override { return false; }
 };
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
 	main_dialog dlg;
-	// TODO: find current mouse position and transfer to dialog
-	if(dlg.do_modal(instance, 0) == IDOK)
+	POINT position;
+	GetCursorPos(&position);
+	dlg.coords = position;
+	if (dlg.do_modal(instance, 0) == IDOK)
 	{
-		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.coords.x, dlg.coords.y);
 	}
 	return 0;
 }
