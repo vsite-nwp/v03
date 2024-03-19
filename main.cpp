fca77e751eff4d787c3f3f251ec645ad4dbb2b10
DrHrast <petar.huljek@gmail.com>
2024-03-19 21:50:33 +0100

    P_huljek
    vježba 3

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <windows.h>
 #include "nwpdlg.h"
 #include "resource.h"
+#include "exception"
+#include "iostream"
 
 class main_dialog : public vsite::nwp::dialog 
 {
+public:
+	POINT point_of_cursor;
+
 protected:
 	int idd() const override
 	{ 
@@ -11,14 +16,24 @@ protected:
 	}
 	bool on_init_dialog() override
 	{
-		// TODO: set initial values to edit controls
+		// DID_IT: set initial values to edit controls
+		dialog::set_int(IDC_EDIT1, point_of_cursor.x);
+		dialog::set_int(IDC_EDIT2, point_of_cursor.y);
 		return true;
 	}
 	bool on_ok() override
 	{
-		// TODO: get current values from edit controls
-		// TODO: if not valid return false
-		return true;
+		// DID_IT: get current values from edit controls
+		// DID_IT: if not valid return false
+		try {
+			point_of_cursor.x = dialog::get_int(IDC_EDIT1);
+			point_of_cursor.y = dialog::get_int(IDC_EDIT2);
+			return true;
+		}
+		catch (const std::runtime_error e){
+			std::cerr << "Error: " << e.what();
+			return false;
+		}
 	}
 	void on_cancel() override { }
 	bool on_command(int id, int code) override { return false; }
@@ -27,10 +42,15 @@ protected:
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
 	main_dialog dlg;
-	// TODO: find current mouse position and transfer to dialog
+	// DID_IT: find current mouse position and transfer to dialog
+
+	POINT cursor_pos;
+	GetCursorPos(&cursor_pos);
+	dlg.point_of_cursor = cursor_pos;
 	if(dlg.do_modal(instance, 0) == IDOK)
 	{
 		// TODO: set mouse position to coordinates from dialog
+		SetCursorPos(dlg.point_of_cursor.x, dlg.point_of_cursor.y);
 	}
 	return 0;
 }
